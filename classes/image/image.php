dca79b42354d1d3335fe063eeb4ed77fa4006e9b
FelTheSheep <57768707+FelTheSheep@users.noreply.github.com>
2019-12-05 15:03:57 -0500

    Add files via upload

--- classes/image/image.php
+++ classes/image/image.php
@@ -1,1 +1,107 @@
-    
+<?php
+
+include_once __DIR__ . "/imagesTDG.PHP";
+
+class Images{
+
+    private $userId;
+    private $imagesId;
+    private $URL;
+    private $description;
+    private $tempsCreation;
+    private $nbView;
+    private $nbLike;
+
+    /*
+        utile si on utilise un factory pattern
+    */
+    public function __construct(){
+        //$this->id = $id;
+        //$this->email = $email;
+        //$this->username = $username;
+        //$this->password = $password;
+        //$this->TDG = new UserTDG;
+    }
+
+
+    //getters
+    public function get_userId(){
+        return $this->userId;
+    }
+
+    public function get_imagesId(){
+        return $this->imagesId;
+    }
+
+    public function get_URL(){
+        return $this->URL;
+    }
+
+    public function get_description(){
+        return $this->description;
+    }
+    public function get_tempsCreation(){
+        return $this->tempsCreation;
+    }
+    public function get_nbLike(){
+        return $this->nbLike;
+    }
+    public function get_nbView(){
+        return $this->nbView;
+    }
+
+
+
+
+    //setters
+    public function set_imagesId($id){
+        $this->imagesId = $id;
+    }
+
+    public function set_URL($URL){
+        $this->URL = $URL;
+    }
+
+    public function set_description($description){
+        $this->description = $description;
+    }
+
+    public function set_tempsCreation($temps){
+        $this->tempsCreation = $temps;
+    }
+
+    public function set_nbLike($n){
+        $this->nbLike = $n;
+    }
+
+    public function set_nbView($n){
+        $this->nbView = $n;
+    }
+
+    /*
+        Quality of Life methods (Dans la langue de shakespear (ou QOLM pour les intimes))
+    */
+    public function load_image($imageId){
+        $TDG = new imagesTDG();
+        $res = $TDG->get_by_imagesId($imageId);
+
+        if(!$res)
+        {
+            $TDG = null;
+            return false;
+        }
+
+        $this->imagesId = $res['imagesId'];
+        $this->userId = $res['userId'];
+        $this->URL = $res['URL'];
+        $this->description = $res['description'];
+        $this->nbView = $res['nbView'];
+        $this->nbLike = $res['nbLike'];
+        $this->tempsCreation = $res['tempsCreation'];
+        
+        $TDG = null;
+        return true;
+    }
+}
+
+
---
+++ classes/image/imageTDG.php
@@ -0,0 +1,143 @@
+<?php
+
+include_once __DIR__ . "/../../utils/connector.php";
+
+class imagesTDG extends DBAO{
+
+    private $tableName;
+    
+    public function __construct(){
+        Parent::__construct();
+        $this->tableName = "images";
+    }
+
+    public function get_by_userId($id){
+        
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM images WHERE userId=:userid";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':userid', $id);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $result;
+    }
+
+
+    public function get_by_imagesId($id){
+        
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM images WHERE imagesId=:imagesId";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':imagesId', $id);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $result;
+    }
+
+    public function add_images($URL, $description, $nbView, $nbLike, $tempsCreation, $userId){
+        
+        try{
+            $conn = $this->connect();
+            //$test = $conn->prepare('INSERT INTO users (email, username, password, image) VALUES (":email", ":username", ":password", ":image")');
+            //$test->execute();
+            $tableName = $this->tableName;
+            $query = "INSERT INTO $tableName (URL, description, nbView, nbLike, tempsCreation, userId) VALUES (:URL, :description, :nbView, :nbLike, :tempsCreation, :userId)";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':URL', $URL);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':nbView', $nbView);
+            $stmt->bindParam(':nbLike', $nbLike);
+            $stmt->bindParam(':tempsCreation', $tempsCreation);
+            $stmt->bindParam(':userId', $userId);
+            $stmt->execute();
+            $resp =  true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp =  false;
+        }
+        $conn = null;
+        return $resp;
+    }
+
+    public function update_description($description, $imagesId){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "UPDATE $tableName SET description=:description WHERE imagesId=:imagesId";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':imagesId', $imagesId);
+            $stmt->execute();
+            $resp = true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        $conn = null;
+        return $resp;
+    }
+
+    public function update_nbLike($nb, $imageId){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "UPDATE $tableName SET nbLike = nbLike + $nb WHERE imageId=:imageId";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':imageId', $imageId);
+            $stmt->execute();
+            $resp = true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        $conn = null;
+        return $resp;
+    }
+    
+    public function delete_images($imageId){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "delete from $tableName where imageId=:imageId";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':imageId', $imageId);
+            $stmt->execute();
+            $resp = true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        $conn = null;
+        return $resp;
+    }
+
+}
