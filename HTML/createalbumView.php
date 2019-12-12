787a85a686bc6bac3e3de6f64ad50d634d1fcee6
yukiriu <50415543+yukiriu@users.noreply.github.com>
2019-12-12 12:58:44 -0500

    Add files via upload

--- HTML/createalbumView.php
+++ HTML/createalbumView.php
@@ -1,6 +1,11 @@
-<div class="container mx-auto xl:max-w-3xl h-full bg-white rounded-lg shadow overflow-hidden">
-    <form>
-    <div class="text-gray-700 text-sm font-semibold ml-4 mt-4"> Title  </div>
-        <input class="text-sm rounded w-11/12 px-4 m-4 text-gray-700 bg-gray-200 h-10" name="title" type="text" placeholder="Summer vacation or sumthing..." />
+<div class="container mx-auto w-auto xl:max-w-xl h-full bg-white rounded-lg shadow overflow-hidden">
+    <form class="w-11/12 pr-auto mr-auto">
+    <div class="text-gray-700 text-sm font-semibold ml-4 mt-4">Title</div>
+        <input class="text-sm rounded w-full p-4 m-4 text-gray-800 bg-gray-200 h-10" name="title" type="text" placeholder="Summer vacation or something..." />
+        <div class="text-gray-700 text-sm font-semibold ml-4"> Description</div>
+        <textarea class="text-sm rounded w-full p-4 m-4 text-gray-800 bg-gray-200 h-64" style="word-break: break-word" name="desc" type="text" placeholder=">:)"></textarea>
+        <button class="w-32 bg-gray-800 hover:bg-gray-400 hover:text-gray-800 text-white text-sm py-2 px-4 ml-4 mb-4 rounded h-10" type="submit">
+          Create album +
+        </button>
     </form>
 </div>
---
+++ classes/album/albumTDG.php
@@ -0,0 +1,159 @@
+<?php
+
+include_once __DIR__ . "/../../utils/connector.php";
+
+class AlbumTDG extends DBAO{
+
+    private $tableName;
+    
+    public function __construct(){
+        Parent::__construct();
+        $this->tableName = "albums";
+    }
+
+
+    public function get_by_id($id){
+        
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM albums WHERE albumID=:albumID";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':albumID', $id);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $result;
+    }
+
+
+    public function get_by_creator($id){
+        
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM albums WHERE userID=:creator";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':creator', $id);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $result;
+    }
+
+    public function get_all_albums(){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "SELECT * FROM $tableName";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+        }
+        
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $result;
+    }
+
+    public function add_album($creatorID,$title, $time, $description, $image){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "INSERT INTO $tableName (userID, title, time, description) VALUES (:creatorID, :title, :time, :description)";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':title', $title);
+            $stmt->bindParam(':time', $time);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':creatorID', $creatorID);
+            $stmt->execute();
+            $resp =  true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp =  false;
+        }
+        $conn = null;
+        return $resp;
+    }
+
+    public function update_title($title, $id){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "UPDATE $tableName SET title=:title WHERE albumID=:albumID";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':title', $title);
+            $stmt->bindParam(':albumID', $id);
+            $stmt->execute();
+            $resp = true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        $conn = null;
+        return $resp;
+    }
+
+    public function update_description($description, $id){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "UPDATE $tableName SET description=:description WHERE albumID=:albumID";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':albumID', $id);
+            $stmt->execute();
+            $resp = true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        $conn = null;
+        return $resp;
+    }
+    public function delete_album($id){
+        
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "DELETE from $tableName  WHERE albumID=:albumID";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':albumID', $id);
+            $stmt->execute();
+            $resp = true;
+        }
+        
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        $conn = null;
+        return $resp;
+    }
+}
